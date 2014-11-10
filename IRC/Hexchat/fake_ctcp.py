b8b57ef7a0f5b60b6d2dabeb22f98f08efd18d3c
Veek M <vek.m1234@gmail.com>
2014-11-10 09:37:50 +0530

    Fix bug where the plugin replies to a message containing TIME

--- IRC/Hexchat/fake_ctcp.py
+++ IRC/Hexchat/fake_ctcp.py
@@ -10,7 +10,7 @@ FAKE_VERSION = 'pinoyChat v1.3.3.4 -  Windows XP SP2,'\
 
 
 def debug(msg):
-    hexchat.prnt('{}'.format(msg))
+    hexchat.prnt('DEBUG: {}'.format(msg))
 
 
 def get_mangled_date():
@@ -40,21 +40,42 @@ def ctcp_reply(nick, cmd, msg):
 
 
 def ctcp_callback(word, word_eol, userdata):
-    sending_nick = extract_sender(word)
+    # Grab the PRIVMSG IRC command
     recv_cmd = word_eol[0]
+    sending_nick = extract_sender(word) # Grab sender of cmd
 
-    if 'VERSION' in recv_cmd:
-       ctcp_reply(sending_nick, 'VERSION', FAKE_VERSION)
-       debug(recv_cmd)
-       return hexchat.EAT_ALL
-    elif 'TIME' in recv_cmd:
-       ctcp_reply(sending_nick, 'TIME', get_mangled_date())
-       debug(recv_cmd)
-       return hexchat.EAT_ALL
-    elif 'PING' in recv_cmd:
-       ctcp_reply(sending_nick, 'PING', 10)
-       debug(recv_cmd)
-       return hexchat.EAT_ALL
+    # Get the start of the PRIVMSG and copy till end into ..frag
+    idx = recv_cmd.index('PRIVMSG')
+    nic_cmd_frag = recv_cmd[idx:]
+
+    # Extract the nick and cmd. If nick is me, then handle dangerous
+    # cmds
+    try:
+        nick, cmd = nic_cmd_frag.split(':', 1)
+    except:
+        debug("ERROR freenode_ctcp.py! PRIVMSG - problem with :")
+        debug(word[0])
+        debug(word_eol[0])
+        return EAT_ALL
+
+    # Obtain current nickname from hexchat cfg
+    mynick = hexchat.get_info('nick')
+    if mynick in nick:
+        if 'VERSION' in cmd:
+           ctcp_reply(sending_nick, 'VERSION', FAKE_VERSION)
+           debug(word_eol)
+           return hexchat.EAT_ALL
+        elif 'TIME' in cmd:
+           ctcp_reply(sending_nick, 'TIME', get_mangled_date())
+           debug(word_eol)
+           return hexchat.EAT_ALL
+        elif 'PING' in cmd:
+           ctcp_reply(sending_nick, 'PING', 10)
+           debug(word_eol)
+           return hexchat.EAT_ALL
+        else:
+            debug(word_eol)
+            return hexchat.EAT_ALL
 
     return hexchat.EAT_NONE
 
