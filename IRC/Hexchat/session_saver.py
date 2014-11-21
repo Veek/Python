29ec509ee51b76bd5931933546f165ac529a4ab4
Veek M <vek.m1234@gmail.com>
2014-11-21 15:10:06 +0530

    Yikes! Ignore this.

--- IRC/Hexchat/session_saver.py
+++ IRC/Hexchat/session_saver.py
@@ -1,4 +1,5 @@
 import hexchat
+import time
 
 __module_name__ = "Session Saver"
 __module_version__ = "0.1"
@@ -6,23 +7,24 @@ __module_description__ = "Periodically saves current session for next start"
 
 
 def debug(msg):
-    msg = str(msg)
-    hexchat.prnt('{}'.format(msg))
+    hexchat.prnt('DEBUG: {}'.format(msg))
 
 #-----------------------------------------------------------------------
 def get_tab_data():
+    '''Returns a list of network= channels'''
     tab_data = {}
     for pref in hexchat.list_pluginpref():
         if len(pref) > 8 and pref[:8] == 'session_':
             network = pref[8:]
             channels = hexchat.get_pluginpref('session_' + network).split(',')
             tab_data[network] = channels
-
     return tab_data
 
 
 def join_channels(channels):
-    delay = hexchat.get_prefs('irc_join_delay') + 10
+    '''Joins the list of channels for a server that's been logged
+    into'''
+    delay = hexchat.get_prefs('irc_join_delay')
     for chan in channels:
         if len(chan) and chan[0] != '#':
             hexchat.command('timer {} query -nofocus {}'.format(delay, chan))
@@ -31,9 +33,10 @@ def join_channels(channels):
 
 
 def load_session():
+    '''Loads a saved session'''
     tab_data = get_tab_data()
+#    debug(tab_data)
     for network in tab_data.keys():
-        hexchat.del_pluginpref('session_' + network)
         if not hexchat.get_context():
             hexchat.command('server irc://"{}"/'.format(network))
             join_channels(tab_data[network])
@@ -41,26 +44,37 @@ def load_session():
             hexchat.command('server irc://"{}"/'.format(network))
             hexchat.find_context(server=network).set()
             join_channels(tab_data[network])
+        hexchat.del_pluginpref('session_' + network)
 
 #-----------------------------------------------------------------------
 def save_session(userdata):
+    '''Saves the network|channels dictionary in pluginpref'''
     networks = {}
+    channels = hexchat.get_list('channels')
 
-    for chan in hexchat.get_list('channels'):
+    # Add all the servers
+    for chan in channels:
+        if chan.type == 1:
+            networks[chan.network] = []
+
+    # Add selected channels. networks={'freenode':[chan1, chan2 key, chan3], 'dalnet':[]}
+    for chan in channels:
         if chan.type == 2 or chan.type == 3: # Ignore notices and server tabs
-            if not chan.network in networks:
-                networks[chan.network] = []  # Append a new empty entry
             if (chan.channelkey):
                 networks[chan.network].append(chan.channel + ' ' + chan.channelkey)
             else:
                 networks[chan.network].append(chan.channel)
 
+    # session_freenode, chan1,cha2 key,chan3
     for network, channels in networks.items():
-        hexchat.set_pluginpref('session_' + network, ','.join(channels))
-
+        if len(network):
+            hexchat.set_pluginpref('session_' + network, ','.join(channels))
+#    debug(networks)
     return hexchat.EAT_ALL
 
-
+def connected():
+    time.sleep(1)
 #-----------------------------------------------------------------------
 load_session()
-hexchat.hook_timer(10000, save_session) # Save every 1 minute
+hexchat.hook_timer(500000, save_session) # Save every x minutes
+# hexchat.hook_unload(save_session) # Doesn't work with X11 Quit
